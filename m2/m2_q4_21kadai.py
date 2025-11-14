02be050e0e179baa88aeee8d657446cc3ff03a25
itc-n24010 <n24010@std.it-college.ac.jp>
2025-11-14 02:44:43 +0000

    kadai_11_14

---
+++ m2/m2_q4_21kadai.py
@@ -0,0 +1,13 @@
+list_a = [4, 8, 3, 7, 9]
+list_b = [10, 3, 6, 7, 2]
+list_c = []
+i = 0
+for i in range(5): #iが5未満の間繰り返し
+    if list_a[i] > list_b[i]:
+        list_c.append(list_a[i] - list_b[i] ** 2)
+    elif list_a[i] == list_b[i]:
+        list_c.append(list_a[i] * 2 % 4)
+    else:
+        list_c.append((list_b[i] - list_a[i]) // 2)
+    i += 1
+print(max(list_c))
