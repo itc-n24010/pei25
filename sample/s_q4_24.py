6775ad428e82e9f007be72d3b357386e19408da4
itc-n24010 <n24010@std.it-college.ac.jp>
2026-01-29 14:52:22 +0000

    kadai_01129

---
+++ sample/s_q4_24.py
@@ -0,0 +1,14 @@
+commands = ['forward', 'back', 'Back', 'turn_left']
+
+for cmd in commands:
+    match cmd:
+        case 'forward':
+            print('前へ移動')
+        case 'back':
+            print('後ろへ移動')
+        case 'turn_right':
+            print('右へ回る')
+        case 'turn_left':
+            print('左へ回る')
+        case x:
+            print(x + 'は存在しないコマンド')
