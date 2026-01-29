5d90775ebc4c9d3122b365f4bed310e032315a5c
itc-n24010 <n24010@std.it-college.ac.jp>
2026-01-29 07:27:17 +0000

    kadai_1024

--- m1_q5_26kadai.py
+++ m1_q5_26kadai.py
@@ -1,9 +1,9 @@
-a = 2
-numbers = []
+a = 2 #aに2を代入する
+numbers = [] #結果を入れるための空のリストを用意する
 
-for i in range(6): 
-    numbers.append(a) 
-    a = a * 2 
+for i in range(6): #6回繰り返して6つ要素を作る
+    numbers.append(a) #appendメソッドで、aの値をリストnumbersの末尾に1つ追加する
+    a = a * 2 #次の値を作るため2倍する
 
 print(numbers)
 
