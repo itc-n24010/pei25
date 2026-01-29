6bc51c31f56e0bd8a5d90d7a3d671b181f674094
itc-n24010 <n24010@std.it-college.ac.jp>
2026-01-29 07:13:17 +0000

    kadai_0129

---
+++ m1_q4_24kadai.py
@@ -0,0 +1,13 @@
+animals = ['Dog', 'Cat', 'Rabbit', 'Horse', 'Dolphin']
+total = 0
+for animal in animals:
+    from_D = animal.startswith('D')
+    is_long = len(animal) > 5
+    print(f"totalの値は{total}") #ここに追加
+    if from_D and is_long:
+        break
+    elif not from_D and is_long:
+        total += animal.find('b')
+    else:
+        total += len(animal)
+print(total)
